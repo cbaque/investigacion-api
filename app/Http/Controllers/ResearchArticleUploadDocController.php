ab2d0168d8ecee42ce74ab628b105e89ffb20133
cbaque <claudio.sys16@gmail.com>
2024-12-21 11:39:24 -0500

    avance carga de documentos

--- app/Http/Controllers/ResearchArticleUploadDocController.php
+++ app/Http/Controllers/ResearchArticleUploadDocController.php
@@ -5,8 +5,40 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 
 class ResearchArticleUploadDocController extends Controller
 {
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
 
+
+        DB::beginTransaction();
+
+        try {
+
+            $user = auth()->user();
+            if (!$request->hasFile('file')) {
+                throw new \RuntimeException('No se envió ningún archivo.');
+            }
+
+            $file = $request->file('file');
+            if (!$file->isValid()) {
+                throw new \RuntimeException('El archivo no es válido.');
+            }
+
+            $filePath = $file->store('uploads', 'public');
+
+
+            DB::commit();
+            return response_data($request, Response::HTTP_CREATED, 'Archivo Investigación creado correctamente.', true);
+
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            return response_data(null, Response::HTTP_BAD_REQUEST , $ex->getMessage() );
+        }
+    }
 }
